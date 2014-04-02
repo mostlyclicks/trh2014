29c8e66cc8537684e4bcecdd24486bffbecad524
Carlos Interone <carlos@mostlyclicks.com>
2014-04-01 21:33:16 -0500

    start responsive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 module ApplicationHelper
+  def main_nav
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.menu_tag = "div class=\"collapse navbar-collapse navbar-inverse\" id=\"main-nav-collapse\""
+    presenter.list_tag = "ul class=\"nav navbar-nav\""
+    presenter.max_depth = 0
+    presenter
+  end
+
+  #def main_nav_nobg
+  #  presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+  #  presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
+  #  presenter.list_tag = "ul class=\"nav navbar-nav pull-right pull-left-xs\""
+  #  presenter.max_depth = 0
+  #  presenter
+  #end
+
+
   def main_nav_nobg
     presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
     presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
